92599b43d05850b04113ffd872f18e21c734c96a
Chris Hoffman <choffman@boostlabs.com>
2018-04-09 22:59:28 -0400

    Edits
    - added fonts
    - added rsvp page
    - embedded rsvpify

---
+++ iframe.js
@@ -0,0 +1,5 @@
+var cssLink = document.createElement("link");
+cssLink.href = "assets/stylesheets/styles.css"; 
+cssLink.rel = "stylesheet"; 
+cssLink.type = "text/css"; 
+frames['#RSVPifyIFrame'].document.head.appendChild(cssLink);
