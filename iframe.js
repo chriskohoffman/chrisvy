31491832e66e225b2d708744453c7af3741f26e3
Chris Hoffman <choffman@boostlabs.com>
2018-04-15 19:16:50 -0400

    Ton of updates, production push

--- iframe.js
+++
@@ -1,5 +0,0 @@
-var cssLink = document.createElement("link");
-cssLink.href = "assets/stylesheets/styles.css"; 
-cssLink.rel = "stylesheet"; 
-cssLink.type = "text/css"; 
-frames['#RSVPifyIFrame'].document.head.appendChild(cssLink);
